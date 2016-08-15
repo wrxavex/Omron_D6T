36fb7db47a98fcd6a4679fae8bc9d3b08bd62c3b
wrxavex <wrxavex@gmail.com>
2016-08-15 15:58:57 +0800

    change temperature target

--- debug_d6t.py
+++ debug_d6t.py
@@ -122,7 +122,7 @@ while True:
     # print(bytes_read)
     if bytes_read == 35:
         print 'Bytes read from Omron D6T: ' + str(bytes_read)
-        print 'Data read from Omron D6T : ' + temperature_data[0]
+        print 'Data read from Omron D6T : ' + str(temperature_data[0])
 
 
 
