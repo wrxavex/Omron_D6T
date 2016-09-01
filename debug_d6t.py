4244ab3c6fd91da096e41bbc76803a7a487d4b53
wrxavex <wrxavex@gmail.com>
2016-09-02 01:24:10 +0800

    change to offical function

--- debug_d6t.py
+++ debug_d6t.py
@@ -191,7 +191,7 @@ while True:
 
         for x in range(16):
             p[x] = tP[x] - tPATA
-            if p[x] > 25:
+            if p[x] > 20:
                 p[x] = 1
             else:
                 p[x] = 0
