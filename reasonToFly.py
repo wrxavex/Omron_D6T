af5fc5aeda114a78a0851681dfd6844482fc5fbf
wrxavex <wrxavex@gmail.com>
2016-12-28 22:35:32 +0800

    add reason to fly

--- reasonToFly.py
+++ reasonToFly.py
@@ -196,13 +196,13 @@ while True:
                         bulbs[s.stage_count].h -= 65535
                     bulbs[s.stage_count].b = 60
 
-                s.stage_count += 1
-                s.stage_show_time = time.time()
-                print('update show_time')
+            s.stage_count += 1
+            s.stage_show_time = time.time()
+            print('update show_time')
 
-                if s.stage_count >= 15:
-                    s.stage_next += 1
-                    s.stage_count = 0
+            if s.stage_count >= 15:
+                s.stage_next += 1
+                s.stage_count = 0
 
         if pirs[0].state == 1:
 
