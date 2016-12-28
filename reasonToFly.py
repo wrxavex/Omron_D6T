02a6e45e11242e9e0c987ca6fd95f9c209efa6e6
wrxavex <wrxavex@gmail.com>
2016-12-28 21:59:14 +0800

    add reason to fly

--- reasonToFly.py
+++ reasonToFly.py
@@ -177,7 +177,8 @@ while True:
     if s.state == 1:
         if time.time() - s.stage_show_time > 0.2:
             print('time to show')
-            print('{0:0d} {0:1d}'.format(bulbs[0].h, bulbs[1].h))
+            print('bulb[0].h:{0:0d} bulb[1].h{0:1d}'.format(bulbs[0].h, bulbs[1].h))
+            print('bulb[0].b:{0:0d} bulb[1].b{0:1d}'.format(bulbs[0].b, bulbs[1].b))
             if bulbs[s.stage_count].state == 0:
 
                 print('bulbs active')
