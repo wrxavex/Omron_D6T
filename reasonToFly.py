22694b776dfee81da9c1f3ea16f68c61da5bb987
wrxavex <wrxavex@gmail.com>
2016-12-29 19:30:06 +0800

    add reason to fly

--- reasonToFly.py
+++ reasonToFly.py
@@ -259,8 +259,8 @@ while True:
 
             s.stage_count += 1
             s.main_color += 100
-            if s.main_color > 65535:
-                s.main_color = 50000
+            if s.main_color > 50000:
+                s.main_color = 20000
             s.stage_show_time = time.time()
             print('update show_time')
 
