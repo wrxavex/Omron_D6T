559c433303a67672b7078a3f02a272b693ffe02e
wrxavex <wrxavex@gmail.com>
2016-12-29 20:35:24 +0800

    add reason to fly

--- reasonToFly.py
+++ reasonToFly.py
@@ -240,7 +240,6 @@ while True:
                 lights[s.stage_count].brightness = bulbs[s.stage_count].b
                 lights[s.stage_count].saturation = bulbs[s.stage_count].s
 
-                lights[s.stage_count].alert()
 
 
                 # if s.stage_next % 2 == 0:
